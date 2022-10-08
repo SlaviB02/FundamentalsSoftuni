4111f8ed0b417720237865653d3b24b30ef1860d
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2022-10-08 16:07:52 +0300

    Add files via upload

---
+++ Exercises/Inventory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split(", ").ToList();
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if(input=="Craft!")
+                {
+                    Console.WriteLine(string.Join(", ", items));
+                    break;
+                }
+                string[] command = input.Split(" - ");
+                if(command[0]=="Collect")
+                {
+                    if(items.Contains(command[1])==false)
+                    {
+                        items.Add(command[1]);
+                    }
+                }
+                if(command[0]=="Drop")
+                {
+                    if(items.Contains(command[1])==true)
+                    {
+                        items.Remove(command[1]);
+                    }
+                }
+                if(command[0]=="Combine Items")
+                {
+                    string[] item = command[1].Split(":");
+                    if(items.Contains(item[0])==true)
+                    {
+                        for(int i=0;i<items.Count;i++)
+                        {
+                            if(item[0]==items[i])
+                             {
+                                items.Insert(i+1, item[1]);
+                            }
+                        }
+                    }   
+                }
+                if(command[0]=="Renew")
+                {
+                    if(items.Contains(command[1])==true)
+                    {
+                        string temp = command[1];
+                        items.Remove(command[1]);
+                        items.Add(temp);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exercises/MuOnline.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 100;
+            int coins = 0;
+            string[] array = Console.ReadLine().Split("|").ToArray();
+            bool flag = true;
+            for(int i=0;i<array.Length;i++)
+            {
+                string[] command = array[i].Split();
+                if(command[0]=="potion")
+                {
+                    int heal = int.Parse(command[1]);
+                    if(health+heal>100)
+                    {
+                        
+                        Console.WriteLine($"You healed for {100-health} hp.");
+                        health = 100;
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                    else
+                    {   
+                        health += heal;
+                        Console.WriteLine($"You healed for {heal} hp.");
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                    
+
+
+                }
+               else if(command[0]=="chest")
+                {
+                    int foundCoins = int.Parse(command[1]);
+                    Console.WriteLine($"You found {foundCoins} bitcoins.");
+                    coins += foundCoins;
+                }
+               else
+                {
+                    int damage = int.Parse(command[1]);
+                    health = health - damage;
+                    if(health>0)
+                    {
+                        Console.WriteLine($"You slayed {command[0]}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {command[0]}.");
+                        Console.WriteLine($"Best room: {i + 1}");
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if(flag==true)
+            {
+                Console.WriteLine("You've made it!");
+                Console.WriteLine($"Bitcoins: {coins}");
+                Console.WriteLine($"Health: {health}");
+            }
+        }
+    }
+}
