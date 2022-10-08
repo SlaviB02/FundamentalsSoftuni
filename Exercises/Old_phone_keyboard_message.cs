2bdebcab4d96f99db3f89248c2920e16878fb9e4
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2022-10-08 10:06:01 +0300

    Add files via upload

---
+++ Exercises/Old_phone_keyboard_message.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string word = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                int tempNumber = number;
+                int count = 0;
+                int mainDigit = 0;
+                int offset = 0;
+                int letterIndex = 0;
+                while (tempNumber > 0)
+                {
+                    mainDigit = tempNumber % 10;
+                    tempNumber /= 10;
+                    count++;
+                }
+                offset = (mainDigit - 2) * 3;
+                if (mainDigit == 8 || mainDigit == 9)
+                {
+                    offset++;
+                }
+                letterIndex = (offset + count - 1);
+                letterIndex += 97;
+                if(mainDigit==0)
+                {
+                    word += " ";
+                    continue;
+                }
+                word += (char)letterIndex;
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
+  
