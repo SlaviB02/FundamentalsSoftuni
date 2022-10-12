52fc7916b7224cb81b49660d143e1ecbe61133e3
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2022-10-12 19:21:32 +0300

    Add files via upload

---
+++ Exercises/ShootForTheWin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = 0;
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if(input=="End")
+                {
+                    Console.Write($"Shot targets: {count} -> ");
+                    Console.Write(string.Join(" ", targets));
+                    break;
+                }
+                int index = int.Parse(input);
+                if(index>=0 && index<targets.Length)
+                {
+                    if(targets[index]!=-1)
+                    {
+                       
+                        for(int i=0;i<targets.Length;i++)
+                        {
+                            if(i!=index &&  targets[i]!=-1)
+                            {
+                                if (targets[i] <= targets[index])
+                                {
+                                    targets[i] += targets[index];
+                                }
+                                else
+                                {
+                                    targets[i] -= targets[index];
+                                }
+                               
+                            }
+                        }
+                        targets[index] = -1;
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
