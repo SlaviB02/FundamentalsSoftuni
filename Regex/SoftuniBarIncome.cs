47c58a328ddf1f2b042da3fb29e1779a57c7a335
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2022-11-04 18:25:15 +0200

    Add files via upload

---
+++ Regex/SoftuniBarIncome.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern =@"^%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<item>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+            double total = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of shift")
+                {
+                    Console.WriteLine($"Total income: {total:F2}");
+                    break;
+                }
+                var matched = Regex.Matches(input, pattern);
+
+                foreach (Match match in matched)
+                {
+
+                    var name = match.Groups["name"].Value;
+                    var price = double.Parse(match.Groups["price"].Value);
+                    var count = int.Parse(match.Groups["count"].Value);
+                    var item = match.Groups["item"].Value;
+                    double totalPrice = price * count;
+                    Console.WriteLine($"{name}: {item} - {totalPrice:F2}");
+                    total += totalPrice;
+                }
+                
+
+            }
+            
+
+
+
+
+        }
+    }
+}
