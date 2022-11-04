63e3f591cfc95df41d6b4604f9f58e8a04537c4e
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2022-11-04 17:30:17 +0200

    Add files via upload

---
+++ Regex/Furniture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(?<furniture>[A-Za-z\s]+)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)";
+            var names = new List<string>();
+            double total = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Purchase")
+                {
+                    break;
+                }
+                var matchedFurniture = Regex.Matches(input, pattern);
+
+                foreach (Match furniture in matchedFurniture)
+                {
+
+                    var name = furniture.Groups["furniture"].Value;
+                    var price = double.Parse(furniture.Groups["price"].Value);
+                    var quantity = int.Parse(furniture.Groups["quantity"].Value);
+                    names.Add(name);
+                    total += price * quantity;
+                }
+
+
+            }
+            Console.WriteLine("Bought furniture:");
+            if (names.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, names));
+            }
+
+            Console.WriteLine($"Total money spend: {total:F2}");
+
+
+
+
+        }
+    }
+}
---
+++ Regex/Race.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternNames = @"[A-Za-z]";
+            Dictionary<string, int> places = new Dictionary<string, int>();
+            string [] names =Console.ReadLine().Split(", ").ToArray();
+            foreach(var person in names)
+            {
+                if(!places.ContainsKey(person))
+                {
+                    places[person] = 0;
+                }
+            }
+            while(true)
+            {
+                string input = Console.ReadLine();
+                string name = "";
+                int km = 0;
+                if(input=="end of race")
+                {
+                    break;
+                }
+                var matchedNames = Regex.Matches(input,patternNames);
+
+                foreach (Match match in matchedNames)
+                {
+
+                    name += match.Value;
+                
+                }
+               
+                if (places.ContainsKey(name))
+                {
+                    string patternDistance = @"[0-9]";
+                    var distance = Regex.Matches(input, patternDistance);
+                    foreach (Match dist in distance)
+                    {
+                        km += int.Parse(dist.Value);
+                    }
+                    places[name] += km;
+                }
+
+            }
+            places = places.OrderByDescending(x => x.Value).ToDictionary(x => x.Key,x => x.Value);
+            Console.WriteLine($"1st place: {places.Keys.ElementAt(0)}");
+            Console.WriteLine($"2nd place: {places.Keys.ElementAt(1)}");
+            Console.WriteLine($"3rd place: {places.Keys.ElementAt(2)}");
+
+
+
+
+        }
+    }
+}
