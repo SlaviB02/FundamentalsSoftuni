ca3fb6c14b121be6aff8ae3d0987f4e2a8cd3e57
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2022-11-05 14:20:00 +0200

    Add files via upload

---
+++ Regex/DestinationMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(=|/)(?<name>[A-Z][A-z]{2,})\1";
+            string input = Console.ReadLine();
+            List<string> cities = new List<string>();
+            MatchCollection destinations = Regex.Matches(input, pattern);
+            int points = 0;
+            foreach(Match city in destinations)
+            {
+                string word = city.Groups["name"].Value;
+                cities.Add(word);
+                points += word.Length;
+            }
+            Console.Write($"Destinations: ");
+            Console.WriteLine(string.Join(", ", cities));
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
